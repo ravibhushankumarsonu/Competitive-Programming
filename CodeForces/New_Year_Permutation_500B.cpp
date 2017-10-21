354b20fa54528701261882ffb3ece11282e6aae0
Ravibhushan Kumar <ravibhushankumar@Ravibhushans-MacBook-Air.local>
2017-10-21 23:41:48 +0530

    New Solution

---
+++ CodeForces/New_Year_Permutation_500B.cpp
@@ -0,0 +1,57 @@
+/*
+Author: Ravibhushan Kumar
+College: IIT(ISM) Dhanbad
+Email id: ravibhushankumarsonu@gmail.com
+Linkdin: https://www.linkedin.com/in/ravibhushan-kumar-6ab881b0/
+github : https://github.com/ravibhushankumarsonu 
+*/
+#include<iostream>
+#include<vector>
+#include<string>
+#include<stack>
+
+
+using namespace std;
+
+void swap(int* a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void sol(vector<string>&graph,vector<int>&arr,int n){
+    vector<bool>visited(n,false);
+    stack<int>st;
+    for(int i=0;i<n;i++){
+        fill(visited.begin(),visited.end(),false);
+        if(!visited[i]){
+            st.push(i);
+            while(!st.empty()){
+                int u=st.top();
+                st.pop();
+                visited[u]=true;
+                for(int j=0;j<n;j++){
+                    if(graph[u][j]=='1'&&visited[j]==false){
+                        if(arr[min(u,j)]>arr[max(u,j)]){
+                            swap(&arr[u],&arr[j]);
+                        }
+                        st.push(j);
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    vector<int>arr(n);
+    vector<string>graph(n);
+    for(int i=0;i<n;i++)scanf("%d",&arr[i]);
+    for(int i=0;i<n;i++)cin>>graph[i];
+    sol(graph,arr,n);
+    for(int i=0;i<n;i++)printf("%d ",arr[i]);
+    printf("\n");
+    return 0;
+}
--- CodeForces/Two_Buttons_520B.cpp
+++ CodeForces/Two_Buttons_520B.cpp
@@ -4,20 +4,31 @@ College: IIT(ISM) Dhanbad
 Email id: ravibhushankumarsonu@gmail.com
 Linkdin: https://www.linkedin.com/in/ravibhushan-kumar-6ab881b0/
 github : https://github.com/ravibhushankumarsonu 
-http://codeforces.com/problemset/problem/159/D
 */
-#include <bits/stdc++.h>
+#include <iostream>
 #define MOD 1000000007
 
 using namespace std;
 
+int sol(int m,int n){
+	if(n==m)return 0;
+	if(n>m){
+		return (n-m);
+	}
+	if(m&1){
+		return 2+sol((m+1)/2,n);
+	}else{
+		return 1+sol(m/2,n);
+	}
+}
 
 int main() {
 	// your code goes here
 	//freopen("input.in","r",stdin);
     //freopen("output.out","w",stdout);
-	string str;
-	cin>>str;
-	cout<<str<<endl;
+    int n,m;
+    cin>>n>>m;
+	int ans=sol(m,n);
+	cout<<ans<<endl;
 	return 0;
 }
