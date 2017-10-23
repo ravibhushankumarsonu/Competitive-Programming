d34c21c84bfd8a06b9e4efb8674bd5b0435282a9
ravibhushankumarsonu <ravibhushankumarsonu@gmail.com>
2017-10-23 13:10:57 +0530

    New Solutions

--- CodeForces/Color_the_Fence_349B.cpp
+++ CodeForces/Color_the_Fence_349B.cpp
@@ -8,13 +8,15 @@ github : https://github.com/ravibhushankumarsonu
 #include <iostream>
 #include<vector>
 #include<algorithm>
-#include<limits>
 #include<string>
 
 #define MOD 1000000007
-
+#define N 1000001
 using namespace std;
-
+/*
+int min(int a,int b){
+	return (a>b)?b:a;
+}*/
 
 int main() {
 	// your code goes here
@@ -23,8 +25,36 @@ int main() {
 	int v;
 	scanf("%d",&v);
 	int arr[10];
+	vector<pair<int,int> >ans;
+	int Min=1000001;
 	for(int i=1;i<10;i++){
 		scanf("%d",&arr[i]);
+		Min=min(Min,arr[i]);
+	}
+	while(v>=Min){
+		//cout<<v<<" ";
+		int Max=v/arr[1];
+		int index=1;
+		for(int i=2;i<10;i++){
+			if((v/arr[i])>=Max){
+				Max=v/arr[i];
+				index=i;
+			}
+		}
+		ans.push_back(make_pair(index,Max));
+		v=v-arr[index]*Max;
+	}
+	if(ans.size()==0){
+		cout<<-1<<endl;
+	}else{
+		for(int i=0;i<ans.size();i++){
+			int x=ans[i].first;
+			int cont=ans[i].second;
+			while(cont--){
+				cout<<x;
+			}
+		}
+		cout<<endl;
 	}
 	return 0;
 }
