b2df40974147318e2b2d23bffc1a26e9a2d76ff4
ravibhushankumarsonu <ravibhushankumarsonu@gmail.com>
2017-11-06 03:16:20 +0530

    New Solutions

---
+++ CodeForces/312B-Archer.cpp
@@ -0,0 +1,28 @@
+/*
+Author: Ravibhushan Kumar
+College: IIT(ISM) Dhanbad
+Email id: ravibhushankumarsonu@gmail.com
+Linkdin: https://www.linkedin.com/in/ravibhushan-kumar-6ab881b0/
+github : https://github.com/ravibhushankumarsonu 
+*/
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<limits>
+#include<string>
+
+#define MOD 1000000007
+
+using namespace std;
+
+
+int main() {
+	// your code goes here
+	//freopen("input.in","r",stdin);
+    //freopen("output.out","w",stdout);
+	long double a,b,c,d;
+	cin>>a>>b>>c>>d;
+	long double ans=(a*d)/(b*d-(b-a)*(d-c));
+	cout<<ans<<endl;
+	return 0;
+}
