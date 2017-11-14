22fe6272c1ffaaa21ca87c9ec10e3cd26f10e649
ravibhushankumarsonu <ravibhushankumarsonu@gmail.com>
2017-11-14 20:45:04 +0530

    New Solutions

--- CodeForces/559A-GeraldHexagon.cpp
+++ CodeForces/559A-GeraldHexagon.cpp
@@ -5,7 +5,6 @@ Email id: ravibhushankumarsonu@gmail.com
 Linkdin: https://www.linkedin.com/in/ravibhushan-kumar-6ab881b0/
 github : https://github.com/ravibhushankumarsonu 
 */
-//
 #include <iostream>
 #include<vector>
 #include<algorithm>
@@ -16,16 +15,15 @@ github : https://github.com/ravibhushankumarsonu
 
 using namespace std;
 
+
 int main() {
 	// your code goes here
 	//freopen("input.in","r",stdin);
     //freopen("output.out","w",stdout);
-	int t;
-	scanf("%d",&t);
-	while(t--){
-		int n,a;
-		scanf("%d %d",&n,&a);
-		string ans=0;
-	}
+	int a1,a2,a3,a4,a5,a6;
+	cin>>a1>>a2>>a3>>a4>>a5>>a6;
+	int k=a1+a2+a3;
+	int ans=k*k-a1*a1-a3*a3-a5*a5;
+	cout<<ans<<endl;
 	return 0;
 }
