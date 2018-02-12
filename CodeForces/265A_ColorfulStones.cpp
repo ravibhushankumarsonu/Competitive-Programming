589b8980cf1d6443ed0dc2828002bbe2fb1e320c
ravibhushankumarsonu <ravibhushankumarsonu@gmail.com>
2018-02-12 19:57:09 +0530

    New Solutions

--- CodeForces/265A_ColorfulStones.cpp
+++ CodeForces/265A_ColorfulStones.cpp
@@ -4,7 +4,7 @@ College: IIT(ISM) Dhanbad
 Email id: ravibhushankumarsonu@gmail.com
 Linkdin: https://www.linkedin.com/in/ravibhushan-kumar-6ab881b0/
 github : https://github.com/ravibhushankumarsonu 
-http://codeforces.com/problemset/problem/735/C
+http://codeforces.com/problemset/problem/265/A
 */
 #include <iostream>
 #include<vector>
@@ -15,36 +15,12 @@ http://codeforces.com/problemset/problem/735/C
 #define MOD 1000000007
 
 using namespace std;
-typedef unsigned long long int lli;
+
 
 int main() {
 	// your code goes here
 	//freopen("input.in","r",stdin);
     //freopen("output.out","w",stdout);
-    lli x;
-	cin>>x;
-	int ans=2;
-	lli a=2;
-	lli b=3;
-	if(x<=3){
-		if(x<2){
-			cout<<0<<endl;
-		}else if(x==2){
-			cout<<1<<endl;
-		}else{
-			cout<<2<<endl;
-		}
-		return 0;
-	}
-	while(1){
-		if(a+b<=x){
-			ans++;
-			a=b;
-			b=a+b;
-		}else{
-			break;
-		}
-	}
-	cout<<ans<<endl;
+    
 	return 0;
 }
